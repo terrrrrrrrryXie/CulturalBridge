a8eacd5441121e968916a616e42b05d84dd81467
terrrrrrrrryXie <2952817228@qq.com>
2024-10-10 03:24:51 +1100

    commit

--- babel.config.js
+++ babel.config.js
@@ -1,5 +1,18 @@
 module.exports = {
   presets: [
-    '@vue/cli-plugin-babel/preset'
+    [
+      '@vue/cli-plugin-babel/preset',
+      {
+        targets: {
+          esmodules: true,
+          browsers: [
+            "last 2 versions",
+            "not dead",
+            "not ie 11",
+            "not < 1%"
+          ]
+        }
+      }
+    ]
   ]
-}
+};
